6001f4e031fdc96f5ddd44dd59d6ae1f1c5f856b
Freddie Witherden <freddie@witherden.org>
2014-09-03 15:16:43 +0100

    License blocks.

--- src/pyr.hpp
+++ src/pyr.hpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of polyquad.
+    Copyright (C) 2014  Freddie Witherden <freddie@witherden.org>
+
+    Polyquad is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Polyquad is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with polyquad.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef POLYQUAD_PYR_HPP_
 #define POLYQUAD_PYR_HPP_
